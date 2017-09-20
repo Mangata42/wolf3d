fee0316f04453e3cc37909a1ce22730518610caa
Nader Ghr <Mangata@Curiosity.local>
2017-09-20 21:10:01 +0200

    damn wild segfault

--- parser/main.c
+++ parser/main.c
@@ -6,32 +6,91 @@
 /*   By: nghaddar <nghaddar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/19 11:41:20 by nghaddar          #+#    #+#             */
-/*   Updated: 2017/09/19 17:49:48 by nghaddar         ###   ########.fr       */
+/*   Updated: 2017/09/20 21:07:52 by nghaddar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
 
-void    compute_size()
+int     **convert_buffer(int **map, char *buffer, int x, int y)
+{
+    int i;
+
+    if (!(map = (int **)malloc(sizeof(int) * y)))
+    {
+        perror("Can't malloc");
+        exit(EXIT_FAILURE);
+    }
+    i = 0;
+    while (i < y)
+    {
+        if (!(map[i] = (int *)malloc(sizeof(int) * x)))
+        {
+            perror("Can't malloc");
+            exit(EXIT_FAILURE);
+        }
+        i++;
+    }
+    for (int a = 0; a < y; a++)
+    {
+        for (int b = 0; b < x; b++)
+            map[a][b] = 24;
+    }
+    return (map);
+}
+
+int    **compute_size(char *buffer)
+{
+   int x;
+   int y;
+   int i;
+   int  **map;
+
+   x = 0;
+   y = 0;
+   i = 0;
+   while (buffer[i] != '\0' && buffer[i] != '\n')
+   {
+       if (ft_isdigit(buffer[i]) == 1)
+            x++;
+        i++;
+   }
+    i = 0;
+    while (buffer[i] != '\0')
+    {
+        (buffer[i] == '\n') ? (y++) : (i=i);
+        i++;
+    }
+    y++;
+    map = convert_buffer(map, buffer, x, y);
+    // for (int a = 0; a < y; a++)
+    // {
+    //     for (int b = 0; b < x; b++)
+    //         printf("%d ", map[a][b]);
+    //     puts("\n");
+    // }
+    return (map);
+}
 
 void    parser(int fd)
 {
-    int     ret;
     char    *line;
     char    *buffer;
+    int     **map;
 
-    ret = 42;
     buffer = malloc(0);
-    while (ret > 0)
+    while ((get_next_line(fd, &line)) > 0)
     {
-        ret = get_next_line(fd, &line);
         buffer = ft_realloc((void **)&buffer, ft_strlen(line) + 2);
         buffer = ft_strcat(buffer, line);
         buffer = ft_strcat(buffer, "\n");
         free(line);
     }
-    buffer[ft_strlen(buffer) - 2] = 0;
-    puts(buffer);
+    buffer[ft_strlen(buffer) - 1] = 0;
+    // puts(buffer);
+    map = compute_size(buffer);
+    free(buffer);
 }
 
 int     main(int argc, char **argv)
@@ -50,6 +109,5 @@ int     main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
     parser(fd);
-    while (1);
     return (0);
 }
